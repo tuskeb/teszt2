0b5956b97602440fe8fcf5d17ca02f0adf79eded
tuskeb <tzuskeb@gmail.com>
2020-09-30 16:00:08 +0200

    Initial commit

---
+++ android/src/hu/teszt/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.teszt.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.teszt.game.TesztGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TesztGame(), config);
+	}
+}
